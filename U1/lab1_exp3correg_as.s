702063fad496a2a3ac8828b93701f6afa2ce119b
Artur <88286816+Artur24061234567890@users.noreply.github.com>
2025-02-01 16:29:17 -0500

    Create lab1_exp3correg_as.s

---
+++ U1/lab1_exp3correg_as.s
@@ -0,0 +1,59 @@
+PROCESSOR 18F57Q43
+#include <xc.inc>
+#include <pic18f57q43.inc>
+#include "cabecera.inc"
+
+#define _XTAL_FREQ 4000000UL
+
+PSECT code, reloc=2, abs
+
+val1    equ 0x0C       ; 00001100B
+val2    equ 0x0A       ; 00001010B
+val3    equ 0x08       ; 00001000B
+
+ORG    000000H
+goto    configuro
+
+ORG    000020H
+configuro:
+    movlb    00H    
+    movlw    60H    
+    movwf    OSCCON1, b
+    movlw    02H    
+    movwf    OSCFRQ, b
+    movlw    40H    
+    movwf    OSCEN, b
+    movlb    04H    
+    bcf      TRISD, 0, b    ; RD0 como output
+    bcf      ANSELD, 0, b   ; RD0 como digital
+    bcf      LATD, 0, b     ; RD0 en 0
+      
+    setf     TRISB, b       ; RB como inputs
+    clrf     ANSELB, b      ; RB en digitales 
+
+inicio:
+    movf    PORTB, w        ; Cargar el estado de PORTB en WREG
+    movwf   val1            ; Mover el valor de PORTB a val1
+    movlw   0x0C            ; Cargar 0x0C en WREG
+    cpfseq  val1            ; Comparar con 00001100B (0x0C)
+    goto    check2          ; Si no es igual, revisar la siguiente
+    bsf     LATD, 0         ; Si es igual, encender la bomba
+    goto    inicio
+
+check2:
+    movlw   0x0A            ; Cargar 0x0A en WREG
+    cpfseq  val1            ; Comparar con 00001010B (0x0A)
+    goto    check3          ; Si no es igual, revisar la siguiente
+    bsf     LATD, 0         ; Si es igual, encender la bomba
+    goto    inicio
+
+check3:
+    movlw   0x08            ; Cargar 0x08 en WREG
+    cpfseq  val1            ; Comparar con 00001000B (0x08)
+    goto    apagar          ; Si no es igual a ninguna, apagar la bomba
+    bsf     LATD, 0         ; Si es igual, encender la bomba
+    goto    inicio
+
+apagar:
+    bcf     LATD, 0         ; Apagar la bomba
+    goto    inicio
