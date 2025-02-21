a4bf4efc60795882302fdaf7adde2123a710d88d
Artur <88286816+Artur24061234567890@users.noreply.github.com>
2025-02-21 09:34:37 -0500

    Create s5_titileo-led_C.c

---
+++ U3/s5_titileo-led_C.c
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<pic18f57q43.h>
+#include<xc.h>
+#include"cabecera.h"
+
+#define _XTAL_FREQ 4000000UL
+
+void configuro();
+
+void main(void)
+{
+    configuro();
+    while(1)
+    {
+        LATFbits.LATF3 = ~LATFbits.LATF3;
+        __delay_ms(200);
+    }    
+}
+
+void configuro()
+{
+    OSCCON1 = 0x60;         //HFINTOSC
+    OSCFRQ = 0x02;          //HFINTOSC a 4MHz
+    OSCEN = 0x40;           //HFINTOSC enable
+    
+    ANSELFbits.ANSELF3 = 0; //RF3: ANALOG SELECTOR DESACTIVADO
+    SLRCONFbits.SLRF3 = 1;  //RF3: SLEW RATE ACTIVADO
+    INLVLFbits.INLVLF3 = 0; //RF3: TRABAJO SOLO CON TTL
+    ODCONFbits.ODCF3 = 0;   //RF3: OPEN-DRAIN DESACTIVADO
+    WPUFbits.WPUF3 = 0;     //RF3: WEAK PULL-UP DESACTIVADO
+    TRISFbits.TRISF3 = 0;   //RF3: TRABAJA COMO OUTPUT
+    //NO SE USA PORTF3 PORQUE ES UN OUTPUT
+    LATFbits.LATF3 = 1;      //RF3: LATCHED 1 (BLOQUEADO EN 1) led interno apagado
+}
